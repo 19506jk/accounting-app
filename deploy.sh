c4619d34dab3237e674ffb776a198dd25f717ee6
19506jk <pengdanny23@gmail.com>
2026-04-06 20:23:11 +0000

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git pull
 npm ci --prefix client
 npm ci --prefix server
 npm run build --prefix client
-npx knex migrate:latest --knexfile server/knexfile.js --env production
-npx knex seed:run --knexfile server/knexfile.js --env production
+npm run migrate --prefix server -- --env production
+npm run seed --prefix server -- --env production
 pm2 restart accounting-app --update-env
